980e26e20241e18b0aa30842ab01f3ceb4bc5e25
KimYongBin <youk8253@gmail.com>
2024-02-28 15:29:32 +0900

    Title: 부분 수열의 합이 m, Time: 114ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240228/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
+	static int n, m;
+	static int[] arr, dp;
+	static void input() throws Exception {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n + 1];
+		dp = new int[m + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.fill(dp, Integer.MAX_VALUE - 1);
+		dp[0] = 0;
+	}
+
+	static void solve() throws Exception {
+		for(int i = 1; i <= n; i++) {
+			for(int j = m; j >= 0; j--) {
+				if(j - arr[i] >= 0) {
+					dp[j] = Math.min(dp[j], dp[j - arr[i]] + 1);
+				}
+			}
+		}
+		
+		if(dp[m] == Integer.MAX_VALUE - 1) {
+			System.out.println(-1);
+		}else {
+			System.out.println(dp[m]);
+		}
+		
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		input();
+		solve();
+	}
+
+}
