11236c5b3c6d114c54c50c3947fc1d26e6aa2e69
KimYongBin <youk8253@gmail.com>
2024-02-28 15:10:04 +0900

    Title: 동전 거슬러주기, Time: 131ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240228/동전 거슬러주기/coin-change.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
+	static int n, m;
+	static int[] arr, dp;
+	static void input() throws Exception {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n + 1];
+		dp = new int[10000 + 1];
+		Arrays.fill(dp, Integer.MAX_VALUE - 1);
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[arr[i]] = 1;
+		}
+		
+		
+	}
+
+	static void solve() throws Exception {
+		for(int i = 2; i <= m; i++) {
+			for(int j = 1; j <= n; j++) {
+				if(i - arr[j] >= 0) {
+					dp[i] = Math.min(dp[i], dp[i - arr[j]] + 1);
+				}
+			}
+		}
+		if(dp[m] == Integer.MAX_VALUE - 1) {
+			System.out.println(-1);
+		}else {
+			System.out.println(dp[m]);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		input();
+		solve();
+	}
+
+}
