90eeed1139ce9be4565ea7163c5d4c8a1ae8711b
KimYongBin <youk8253@gmail.com>
2025-04-15 16:52:21 +0900

    Title: 길이 단위 환산하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250415/길이 단위 환산하기/convert-length-units.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    double ft = 30.48f;
+    double input;
+    scanf("%lf", &input);
+    printf("%lf", input * ft);
+    return 0;
+}
