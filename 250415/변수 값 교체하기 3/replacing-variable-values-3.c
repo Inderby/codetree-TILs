b8b4ff85876add38e36f0feefc98bcf3235f5086
KimYongBin <youk8253@gmail.com>
2025-04-15 15:59:46 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250415/변수 값 교체하기 3/replacing-variable-values-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 5;
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("$d\n", a);
+    printf("$d", b);
+    return 0;
+}
