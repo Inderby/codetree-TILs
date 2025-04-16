83693b944b9291d32cf52f3b676887749ae60370
KimYongBin <youk8253@gmail.com>
2025-04-16 19:11:13 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250416/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.c
+++ 250416/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.c
@@ -4,7 +4,7 @@ int main() {
     // Please write your code here.
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("%d * %d = %d", a, b, a*b);
+    printf("%d * %d = %d\n", a, b, a*b);
     printf("%d / %d = %d", a, b, a/b);
     return 0;
 }
