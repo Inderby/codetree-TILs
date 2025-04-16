70f939d48a4097836095df6f44aaf5ddcf8d9015
KimYongBin <youk8253@gmail.com>
2025-04-16 19:05:58 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250416/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int width, height;
+    scanf("%d %d", &width, &height);
+    a += 8;
+    b *= 3;
+    printf("%d\n%d\n%d", width, height, width * height);
+    return 0;
+}
