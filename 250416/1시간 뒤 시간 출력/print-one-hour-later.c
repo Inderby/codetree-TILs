191ee88af1305adad0f7286f861cdf1de439bdac
KimYongBin <youk8253@gmail.com>
2025-04-16 14:15:43 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250416/1시간 뒤 시간 출력/print-one-hour-later.c
+++ 250416/1시간 뒤 시간 출력/print-one-hour-later.c
@@ -3,8 +3,8 @@
 int main() {
     // Please write your code here.
     int h,m;
-    scanf("%d %d", &h, &m);
-    printf("%d:%d", h, m);
+    scanf("%d:%d", &h, &m);
+    printf("%d:%d", h + 1, m);
 
     return 0;
 }
