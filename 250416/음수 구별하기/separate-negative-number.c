1c228daf4eea740a6272f45044c70118865d5b67
KimYongBin <youk8253@gmail.com>
2025-04-16 19:40:39 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250416/음수 구별하기/separate-negative-number.c
+++ 250416/음수 구별하기/separate-negative-number.c
@@ -4,7 +4,7 @@ int main() {
     // Please write your code here.
     int a;
     scanf("%d", &a);
-    printf("%d", a);
+    printf("%d\n", a);
     if(a < 0)
         printf("minus");
     return 0;
