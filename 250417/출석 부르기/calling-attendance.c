bd37fa2f36d2d19ba5e2b0b41930c08c2de34140
KimYongBin <youk8253@gmail.com>
2025-04-17 20:26:20 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250417/출석 부르기/calling-attendance.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d", &a);
+    if(a == 1){
+        printf("John");
+    }else if(a == 2){
+        printf("Tom");
+    }else{
+        printf("Paul");
+    }
+    return 0;
+}
