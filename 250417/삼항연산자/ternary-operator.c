237f45aacaff5dd29bdf921da57260c0b4888207
KimYongBin <youk8253@gmail.com>
2025-04-17 20:05:16 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250417/삼항연산자/ternary-operator.c
+++ 250417/삼항연산자/ternary-operator.c
@@ -4,6 +4,6 @@ int main() {
     // Please write your code here.
     int score;
     scanf("%d", &score);
-    printf("%s", score == 100 ? "pass", "failure");
+    printf("%s", score == 100 ? "pass" : "failure");
     return 0;
 }
