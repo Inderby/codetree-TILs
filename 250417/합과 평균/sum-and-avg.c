ceb06f6ae68e9eae90eabd30a3a033479d352a31
KimYongBin <youk8253@gmail.com>
2025-04-17 19:40:01 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250417/합과 평균/sum-and-avg.c
+++ 250417/합과 평균/sum-and-avg.c
@@ -3,7 +3,7 @@
 int main() {
     // Please write your code here.
     int a, b;
-    scanf("%d %d", a, b);
+    scanf("%d %d", &a, &b);
     printf("%d %.1lf", a + b, (double)(a + b) / 2);
     return 0;
 }
