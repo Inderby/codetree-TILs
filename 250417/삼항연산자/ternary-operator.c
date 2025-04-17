fb12488fc4c7653b6387fab24769e7251a2c38f0
KimYongBin <youk8253@gmail.com>
2025-04-17 20:04:46 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250417/삼항연산자/ternary-operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int score;
+    scanf("%d", &score);
+    printf("%s", score == 100 ? "pass", "failure");
+    return 0;
+}
