10cd6355caca5701914cf7f0a430d181233dae73
KimYongBin <youk8253@gmail.com>
2025-04-18 08:44:17 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250418/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
+++ 250418/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -5,15 +5,15 @@ int main() {
     int a, b;
     scanf("%d %d", &a, &b);
     if(a < b){
-        printf("1\n");
+        printf("1 ");
     }else{
-        printf("0\n");
+        printf("0 ");
     }
 
     if(a == b){
-        printf("1\n");
+        printf("1");
     }else{
-        printf("0\n");
+        printf("0");
     }
     return 0;
 }
