352c269a610a622ddeeea4205ec28cfa97c746fc
KimYongBin <youk8253@gmail.com>
2025-04-17 20:16:06 +0900

    Title: 살 수 있는 물건, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250417/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+
+    if(N >= 3000){
+        printf("book");
+    }else{
+        printf("mask");
+    }
+    return 0;
+}
