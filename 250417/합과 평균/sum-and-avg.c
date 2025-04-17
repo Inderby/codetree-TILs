723a2dee5f42fcdbb2e6b77710374270228b3099
KimYongBin <youk8253@gmail.com>
2025-04-17 19:39:36 +0900

    Title: 합과 평균, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 250417/합과 평균/sum-and-avg.c
+++ 250417/합과 평균/sum-and-avg.c
@@ -4,6 +4,6 @@ int main() {
     // Please write your code here.
     int a, b;
     scanf("%d %d", a, b);
-    pritnf("%d %.1lf", a + b, (double)(a + b) / 2);
+    printf("%d %.1lf", a + b, (double)(a + b) / 2);
     return 0;
 }
