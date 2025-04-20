ca4b9af730c44bf5e8fc5dc3630d0201a23a9b10
Dante <youk8253@gmail.com>
2025-04-21 08:20:35 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250421/반복 출력하기/repeated-output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void func(int row_num){
+    for(int i = 0; i < row_num; i++){
+        printf("12345^&*()_\n");
+    }
+}
+
+int main() {
+    int row_num;
+    scanf("%d", &row_num);
+    // Please write your code here.
+    return 0;
+}
