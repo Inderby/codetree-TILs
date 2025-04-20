8fabf81c8c804236b0436d6e81f9c134e8780cb9
Dante <youk8253@gmail.com>
2025-04-21 08:26:05 +0900

    Title: 두 정수 값 교환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250421/두 정수 값 교환하기/to-exchange-two-integer-values.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &n);
+    printf("%d %d", n, m);
+    return 0;
+}
