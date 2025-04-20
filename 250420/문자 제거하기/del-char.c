fee3f0308a19ed52cc7c3f78c7a56028bf0c8def
Dante <youk8253@gmail.com>
2025-04-20 13:07:17 +0900

    Title: 문자 제거하기, Time: 1028ms, Memory: 27MB, Status: Time Limit Exceed - Codetree

---
+++ 250420/문자 제거하기/del-char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    // Please write your code here.
+    char s[21];
+    scanf("%s", s);
+    while(strlen(s) != 1){
+        int idx;
+        scanf("%d", &idx);
+        if(strlen(s) < idx){
+            idx = strlen(s) - 1;
+        }
+        int i = idx;
+        for(; i < strlen(s) - 1; i++){
+            s[i] = s[i + 1];
+        }
+        s[i] = '\0';
+        printf("%s\n", s);
+    }
+    return 0;
+}
