3186ece886a8de5c1db95ecc5b4c8f7cdac5d5a1
Dante <youk8253@gmail.com>
2025-04-21 19:58:11 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250421/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void func(int n){
+    if(n == 0){
+        return;
+    }
+    for(int i = 0; i < n; i++){
+        printf("* ");
+    }
+    printf("\n");
+    func(n-1);
+    for(int i = 0; i < n; i++){
+        printf("* ");
+    }
+    printf("\n");
+    return;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    func(n);
+    return 0;
+}
